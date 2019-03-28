0a9cb00412eb5aaf0f8259e08cd33115dd408932
Reza Lotfi <reza.lotfi14@gmail.com>
2019-03-28 14:10:08 -0700

    Implement a 3rd jest test

--- tests/sum.test.js
+++ tests/sum.test.js
@@ -7,3 +7,7 @@ test('adds 1 + 2 to equal 3', () => {
 test('adds 2 + 2 to equal 5', () => {
   expect(sum(2, 2)).toBe(5);
 });
+
+test('adds 3 + 3 to equal 6', () => {
+  expect(sum(3, 3)).toBe(6);
+});
