417e4309d73dade449a7f9f7d4a0ed6de5eecb56
Reza Lotfi <reza.lotfi14@gmail.com>
2019-04-10 19:16:39 -0700

    Change tests for new Modal app view

--- tests/App.test.js
+++ tests/App.test.js
@@ -3,29 +3,11 @@ import React from 'react';
 import { shallow } from 'enzyme';
 
 import App from '../client/src/components/App.jsx';
-import LeftArrow from '../client/src/components/LeftArrow.jsx';
-import RightArrow from '../client/src/components/RightArrow.jsx';
-import Slide from '../client/src/components/Slide.jsx';
 import Thumbnail from '../client/src/components/Thumbnail.jsx';
 
 describe('Component Test Suite', () => {
   it('App should exist', () => {
     const wrapper = shallow(<App />);
-    expect(wrapper.exists()).toBe(true);
-  });
-
-  it('LeftArrow should exist', () => {
-    const wrapper = shallow(<LeftArrow />);
-    expect(wrapper.exists()).toBe(true);
-  });
-
-  it('RightArrow should exist', () => {
-    const wrapper = shallow(<RightArrow />);
-    expect(wrapper.exists()).toBe(true);
-  });
-
-  it('Slide should exist', () => {
-    const wrapper = shallow(<Slide />);
     expect(wrapper.exists()).toBe(true);
   });
 
